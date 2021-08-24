9aaaef6b7a4d5fdaf9484803662361fea9cce1b7
thiagohgmello1 <thiagohgmello@gmail.com>
2021-08-23 22:02:51 -0300

    Create function to calculate liquid irradiation, databases insertion and requirementes update

--- controllers/pvsystem.py
+++ controllers/pvsystem.py
@@ -6,15 +6,16 @@ from datetime import date as d
 from pvlib import solarposition, irradiance
 from pvlib import location as loc
 from controllers.location import Location
-from shading import Shading
+from controllers.shading import Shading
 from controllers.date import Date
 
 
 class PvSystem:
     def __init__(self, date: Date, location: Location, shading: Shading, dates=None,
-                 irrad_path='data_base/data_base.xlsx'):
+                 irrad_path='data_bases/irradiation_curve.xlsx',
+                 solarimetric_path='data_bases/solarimetric_database.xlsx'):
         if dates is None:
-            dates = ['2021-08-21']
+            dates = ['2021-08-21', '2021-06-21', '2021-12-21']
 
         self.date = date
         self.location = location
@@ -22,9 +23,10 @@ class PvSystem:
         self.shading = shading.shading
         self.solar_pos = self._calculate_shading_influence(self.solar_pos)
         self.irradiation = self.get_irradiance1(irrad_path)
+        self.df_solarimetric_data = self.read_solarimetric_data_base(solarimetric_path)
         self.total_radiation = self.integral(y=self.irradiation.Irrad_med)
-        # dates = None
         self.df_utilization_factor = self.utilization_factor_calculator(dates=dates)
+        self.liquid_irradiation = self.calculate_liquid_irradiation_to_specific_dates(self.df_utilization_factor)
         print('oi')
 
 
@@ -101,19 +103,19 @@ class PvSystem:
 
     def plot_polar_chart_with_shading(self, freq):
         ax = plt.subplot(1, 1, 1, projection='polar')
-        self.solar_pos = self._data_preprocessing(self.solar_pos)
+        df_solar_pos = self._data_preprocessing(self.solar_pos)
 
         # Draw the analemma loops
-        points = ax.scatter(np.radians(self.solar_pos.azimuth), self.solar_pos.apparent_zenith,
-                            s=2, label=None, c=self.solar_pos.index.dayofyear)
+        points = ax.scatter(np.radians(df_solar_pos.azimuth), df_solar_pos.apparent_zenith,
+                            s=2, label=None, c=df_solar_pos.index.dayofyear)
         ax.figure.colorbar(points)
 
         # Draw hour labels
-        for hour in np.unique(self.solar_pos.index.hour):
+        for hour in np.unique(df_solar_pos.index.hour):
             # choose label position by the smallest radius for each hour
-            subset = self.solar_pos.loc[self.solar_pos.index.hour == hour, :]
+            subset = df_solar_pos.loc[df_solar_pos.index.hour == hour, :]
             r = subset.apparent_zenith
-            pos = self.solar_pos.loc[r.idxmin(), :]
+            pos = df_solar_pos.loc[r.idxmin(), :]
             ax.text(np.radians(pos['azimuth']), pos['apparent_zenith'], str(hour))
 
         # Draw individual days
@@ -247,7 +249,7 @@ class PvSystem:
         if dates is None:
             dates = pd.date_range(start=d(2021, 1, 1), end=d(2021, 12, 31)).to_pydatetime().tolist()
         irradiance_shading = np.array([])
-        df_utilization_factor = pd.DataFrame(pd.to_datetime(dates).strftime('%m/%d/%Y'), columns=['day'])
+        df_utilization_factor = pd.DataFrame(pd.to_datetime(dates).strftime('%d/%m/%Y'), columns=['day'])
 
         for date in dates:
             y = self._day_irradiance_shading(date, freq)
@@ -265,6 +267,36 @@ class PvSystem:
         df_solar_pos = solarposition.get_solarposition(times, self.location.lat, self.location.lon)
         df_solar_pos = df_solar_pos.loc[df_solar_pos['apparent_elevation'] > 0, :]
         return df_solar_pos
+
+
+    def search_mean_irradiation(self):
+        b_lon = self.location.lon
+        b_lat = self.location.lat
+        tol_lon = 0.05
+        tol_lat = 0.05
+        lon = self.df_solarimetric_data.lon.apply(np.isclose, b=b_lon, atol=tol_lon, rtol=0)
+        lat = self.df_solarimetric_data.lat.apply(np.isclose, b=b_lat, atol=tol_lat, rtol=0)
+        index = [i for i, x in enumerate(lon & lat) if x]
+        return self.df_solarimetric_data.iloc[index]
+
+
+    def calculate_liquid_irradiation_to_specific_dates(self, df_utilization_factor):
+        dates = df_utilization_factor.day
+        formatted_dates = [datetime.strptime(date, '%Y/%m/%d') for date in dates]
+        months = [date.strftime('%b').lower() for date in formatted_dates]
+        mean_irradiation = self.search_mean_irradiation()
+        irrad = mean_irradiation[months].T
+        liquid_irradiance = dict()
+        for i in np.arange(0, df_utilization_factor.shape[0]):
+            liquid_irradiance[dates[i]] = (df_utilization_factor.utilization_factor.iloc[i] * irrad.iloc[i].values[0])
+
+        return pd.DataFrame.from_dict(liquid_irradiance, orient='index', columns=['liquid_irradiation'])
+
+
+    @staticmethod
+    def read_solarimetric_data_base(solarimetric_path):
+        df_solarimetric_path = pd.read_excel(solarimetric_path)
+        return df_solarimetric_path
 
 
     @staticmethod
